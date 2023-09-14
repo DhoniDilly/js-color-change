15f55b3c7f99a191ab4c3c3c84b3a471757b0329
DhoniDilly <dilly0141@gmail.com>
2023-09-14 17:13:40 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+/*  typing animation   */
+
+var typed = new Typed('.typing', {
+    strings:["","web Designer","web Developer","Graphic Designer","YouTuber"],
+    typeSpeed:100,
+    Backspeed:60,
+    loop:true
+})
